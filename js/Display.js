86f90c642cdbf80e9e341f5473f53a7a8e147d2d
Alejandra <amzita@scl.edu.gt>
2023-05-18 16:48:35 -0600

    Historial de la calculadora

---
+++ js/Display.js
@@ -0,0 +1,24 @@
+let key = [
+    "AC", "(", ")", "C",'<img src="./ima" class="delete">',
+    7, 8, 9, "/", "√",
+    4, 5, 6, "*","x²",
+    1,2,3,"-","=",
+    0,".","%","+"
+];
+
+function render_keys(){
+    let dom_keyboard = document.querySelector(".teclado");
+
+    key.forEach((each_key)=>{
+        let div = document.createElement("div");
+        div.innerHTML = each_key;
+        div.classList.add("key_item");
+
+        key.forEach((each_key2,indice)=>{
+            each_key == indice ? div.classList.add("key_tiem_color1") : null
+        });
+        each_key == "=" ? div.classList.add("key_tiem_color2") : null
+
+        dom_keyboard.appendChild(div);
+    });
+}
---
+++ js/main.js
@@ -0,0 +1,48 @@
+render_keys();
+
+let results = document.querySelector(".resultado");
+
+function add_key(){
+    let val = this.innerHTML;
+    results = document.querySelector(".resultado");
+    results.value = results.value + val;
+}
+
+function reset_operation(){
+    results = document.querySelector(".resultado");
+    results.value = "";
+}
+
+function reset_all(){
+    let results2 = document.querySelector(".resultado");
+    results2.value = "0";
+    document.querySelector(".record").innerHTML = "";
+}
+function delete_letter(){
+    var content = results.value;
+    var nuevoValor = content.slice(0, -1);
+    results.value = nuevoValor;
+}
+
+function calculate(){
+    operation = document.querySelector(".resultado");
+    let r = eval(operation.value);
+    set_record(operation.value,r);
+    operation.value = r;
+}
+
+let keys_dom = document.querySelectorAll(".key_item");
+
+keys_dom.forEach((keys_content) => {
+    let content = keys_content.innerHTML;
+
+    if (content == "C") {
+        keys_content.addEventListener("click", reset_operation);
+    } else if (content == "=") {
+        keys_content.addEventListener("click", calculate);
+    } else if (content == "AC") {
+        keys_content.addEventListener("click", reset_all);
+    } else {
+        keys_content.addEventListener("click", add_key);
+    }
+});
